0e3f9b1c34ca03df894d9603b0da4407fd31df29
aaliyahrichlen <aaliyahrichlen@ufl.edu>
2020-11-11 21:00:40 -0500

    protoptye stuff

---
+++ sensor-with-camera/test.py
@@ -0,0 +1,182 @@
+from picamera import PiCamera
+from time import sleep
+from scipy.spatial import distance as dist
+from imutils import perspective
+from imutils import contours
+import numpy as np
+import argparse
+import imutils
+import cv2
+import RPi.GPIO as GPIO
+
+def midpoint(ptA, ptB):
+    return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+
+#from https://stackoverflow.com/questions/35180764/opencv-python-image-too-big-to-display
+def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    return cv2.resize(image, dim, interpolation=inter)
+
+def sort_contours(cnts, method="left-to-right"):
+    # initialize the reverse flag and sort index
+    reverse = False
+    i = 0
+    # handle if we need to sort in reverse
+    if method == "right-to-left" or method == "bottom-to-top":
+        reverse = True
+    # handle if we are sorting against the y-coordinate rather than
+    # the x-coordinate of the bounding box
+    if method == "top-to-bottom" or method == "bottom-to-top":
+        i = 1
+    # construct the list of bounding boxes and sort them from top to
+    # bottom
+    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
+    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
+        key=lambda b:b[1][i], reverse=reverse))
+    # return the list of sorted contours and bounding boxes
+    return (cnts, boundingBoxes)
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.IN)         #Read output from PIR motion sensor
+camera = PiCamera()
+camera.resolution = (2592, 1944)
+camera.framerate = 15
+i = GPIO.input(8)
+
+while i == 0:
+    i = GPIO.input(8)
+    if i == 1:               #When output from motion sensor is HIGH
+        camera.start_preview()
+        sleep(5)
+        camera.capture('image.jpg')
+        camera.stop_preview()
+        
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+#ap.add_argument("-i", "--image", required=True,
+    #help="path to the input image")
+ap.add_argument("-w", "--width", type=float, required=True,
+    help="width of the left-most object in the image (in inches)")
+args = vars(ap.parse_args())
+
+# load the image, convert it to grayscale, and blur it slightly
+image = cv2.imread('image.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+# perform edge detection, then perform a dilation + erosion to
+# close gaps in between object edges
+edged = cv2.Canny(gray, 50, 100)
+edged = cv2.dilate(edged, None, iterations=1)
+edged = cv2.erode(edged, None, iterations=1)
+
+# find contours in the edge map
+cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+    cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+
+num = str(len(cnts))
+print(num + ' object(s) detected')
+
+# sort the contours from left-to-right and initialize the
+# 'pixels per metric' calibration variable
+#(cnts, _) = contours.sort_contours(cnts)
+
+#cnts = sorted(cnts, key=cv2.contourArea, reverse=True) [:2]
+
+(cnts, boundingBoxes) = sort_contours(cnts, method="top-to-bottom")
+cnts = cnts [:2]
+
+pixelsPerMetric = None
+
+i = 0
+
+# loop over the contours individually
+for c in cnts:    
+    # if the contour is not sufficiently large, ignore it
+    if cv2.contourArea(c) < 100:
+        continue
+
+    # compute the rotated bounding box of the contour
+    orig = image.copy()
+    box = cv2.minAreaRect(c)
+    box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+    box = np.array(box, dtype="int")
+
+    # order the points in the contour such that they appear
+    # in top-left, top-right, bottom-right, and bottom-left
+    # order, then draw the outline of the rotated bounding
+    # box
+    box = perspective.order_points(box)
+    cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+
+    # loop over the original points and draw them
+    for (x, y) in box:
+        cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+
+    # unpack the ordered bounding box, then compute the midpoint
+    # between the top-left and top-right coordinates, followed by
+    # the midpoint between bottom-left and bottom-right coordinates
+    (tl, tr, br, bl) = box
+    (tltrX, tltrY) = midpoint(tl, tr)
+    (blbrX, blbrY) = midpoint(bl, br)
+
+    # compute the midpoint between the top-left and top-right points,
+    # followed by the midpoint between the top-righ and bottom-right
+    (tlblX, tlblY) = midpoint(tl, bl)
+    (trbrX, trbrY) = midpoint(tr, br)
+
+    # draw the midpoints on the image
+    cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+    cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+    cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+    cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+
+    # draw lines between the midpoints
+    cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
+        (255, 0, 255), 2)
+    cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
+        (255, 0, 255), 2)
+
+    # compute the Euclidean distance between the midpoints
+    dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+    dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+
+    # if the pixels per metric has not been initialized, then
+    # compute it as the ratio of pixels to supplied metric
+    # (in this case, inches)
+    if pixelsPerMetric is None:
+        pixelsPerMetric = dB / args["width"]
+
+    # compute the size of the object
+    dimA = dA / pixelsPerMetric
+    dimB = dB / pixelsPerMetric
+
+    # draw the object sizes on the image
+    cv2.putText(orig, "{:.2f}mm".format(dimA),
+        (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_TRIPLEX,
+        4, (255, 255, 255), 2)
+    cv2.putText(orig, "{:.2f}mm".format(dimB),
+        (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_TRIPLEX,
+        4, (255, 255, 255), 2)
+
+    # show  the output image
+    resize = ResizeWithAspectRatio(orig, height=540)
+    cv2.imshow("Image", resize)
+    i += 1
+    num = str(i)
+    cv2.imwrite('out' + num + '.jpg', resize)
+    cv2.waitKey(0)
