591fcb3cea45d071bbd741cfc1c4d98a1376ba49
dmackey199 <36615673+dmackey199@users.noreply.github.com>
2021-02-08 05:10:44 -0500

    .

--- pixel-count/rect.py
+++ pixel-count/rect.py
@@ -1,44 +1,46 @@
-import numpy as np
-import cv2 
+import cv2
 
-# Making The Blank Image
-image = np.zeros((512,512,3))
-drawing = False
-ix = 0
-iy = 0
-# Adding Function Attached To Mouse Callback
-def draw(event,x,y,flags,params):
-    global ix,iy,drawing
-    # Left Mouse Button Down Pressed
-    if(event==1):
-        drawing = True
-        ix = x
-        iy = y
-    if(event==0):
-        if(drawing==True):
-            #For Drawing Line
-            cv2.line(image,pt1=(ix,iy),pt2=(x,y),color=(255,255,255),thickness=3)
-            ix = x
-            iy = y
-            # For Drawing Rectangle
-            # cv2.rectangle(image,pt1=(ix,iy),pt2=(x,y),color=(255,255,255),thickness=3)
-    if(event==4):
-        drawing = False
+refPt = []
+cropping = False
+def click_and_crop(event, x, y, flags, param):
+	# grab references to the global variables
+	global refPt, cropping
+	# if the left mouse button was clicked, record the starting
+	# (x, y) coordinates and indicate that cropping is being
+	# performed
+	if event == cv2.EVENT_LBUTTONDOWN:
+		refPt = [(x, y)]
+		cropping = True
+	# check to see if the left mouse button was released
+	elif event == cv2.EVENT_LBUTTONUP:
+		# record the ending (x, y) coordinates and indicate that
+		# the cropping operation is finished
+		refPt.append((x, y))
+		cropping = False
+		# draw a rectangle around the region of interest
+		cv2.rectangle(image, refPt[0], refPt[1], (0, 255, 0), 2)
+		cv2.imshow("image", image)
 
-
-
-# Making Window For The Image
-cv2.namedWindow("Window")
-
-# Adding Mouse CallBack Event
-cv2.setMouseCallback("Window",draw)
-
-# Starting The Loop So Image Can Be Shown
-while(True):
-
-    cv2.imshow("Window",image)
-
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
-
+image = cv2.imread("circles.png",0)
+clone = image.copy()
+cv2.namedWindow("image")
+cv2.setMouseCallback("image", click_and_crop)
+# keep looping until the 'q' key is pressed
+while True:
+	# display the image and wait for a keypress
+	cv2.imshow("image", image)
+	key = cv2.waitKey(1) & 0xFF
+	# if the 'r' key is pressed, reset the cropping region
+	if key == ord("r"):
+		image = clone.copy()
+	# if the 'c' key is pressed, break from the loop
+	elif key == ord("c"):
+		break
+# if there are two reference points, then crop the region of interest
+# from teh image and display it
+if len(refPt) == 2:
+	roi = clone[refPt[0][1]:refPt[1][1], refPt[0][0]:refPt[1][0]]
+	cv2.imshow("ROI", roi)
+	cv2.waitKey(0)
+# close all open windows
 cv2.destroyAllWindows()
