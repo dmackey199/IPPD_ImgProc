139a1620533ce9fd7ac98360a4b5a25ebe9602c3
dmackey199 <36615673+dmackey199@users.noreply.github.com>
2021-02-13 00:47:04 -0500

    trying canny edge detection

--- pixel-count/edge.py
+++ pixel-count/edge.py
@@ -3,20 +3,43 @@
 import cv2
 import matplotlib.pyplot as plt
 
+# # read the image
+# image=cv2.imread("circles.png",0)
+# # convert to RGB
+# #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+# # convert to grayscale
+# #gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+# gray = cv2.medianBlur(image, 5)
+# # create a binary thresholded image
+# _, binary = cv2.threshold(gray, 170, 255, cv2.THRESH_BINARY_INV)
+# # show it
+# plt.imshow(binary, cmap="gray")
+# plt.show()
+# # find the contours from the thresholded image
+# contours, hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+# # draw all contours
+# image = cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+# # show the image with the drawn contours
+# plt.imshow(image)
+# plt.show()
+
+
+#CANNY
 # read the image
 image=cv2.imread("circles.png",0)
-# convert to RGB
+# # convert to RGB
 #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 # convert to grayscale
 #gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 gray = cv2.medianBlur(image, 5)
+edged = cv2.Canny(gray, 30, 200) 
 # create a binary thresholded image
-_, binary = cv2.threshold(gray, 170, 255, cv2.THRESH_BINARY_INV)
+# _, binary = cv2.threshold(gray, 170, 255, cv2.THRESH_BINARY_INV)
 # show it
-plt.imshow(binary, cmap="gray")
+plt.imshow(edged, cmap="gray")
 plt.show()
 # find the contours from the thresholded image
-contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 # draw all contours
 image = cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
 # show the image with the drawn contours
