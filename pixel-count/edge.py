773dfafd1bc401d6ec0dbc3b1a935074afa0eeb3
dmackey199 <36615673+dmackey199@users.noreply.github.com>
2021-02-20 03:20:33 -0500

    finding area

--- pixel-count/edge.py
+++ pixel-count/edge.py
@@ -59,28 +59,9 @@ image = cv2.drawContours(img, sorted_cnts, -1, (0, 127, 0), 2)
 # show the image with the drawn contours
 plt.imshow(image)
 plt.show()
-# print(contours[0])
 
+refArea = cv2.contourArea(sorted_cnts[0])
+objArea = cv2.contourArea(sorted_cnts[0])
 
-# #CANNY
-# # read the image
-# image=cv2.imread("circles.png",0)
-# # # convert to RGB
-# #image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-# # convert to grayscale
-# #gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-# gray = cv2.medianBlur(image, 5)
-# edged = cv2.Canny(gray, 30, 200) 
-# cv2.waitKey(0) 
-# # create a binary thresholded image
-# # _, binary = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
-# # show it
-# plt.imshow(edged, cmap="gray")
-# plt.show()
-# # find the contours from the thresholded image
-# contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-# # draw all contours
-# image = cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
-# # show the image with the drawn contours
-# plt.imshow(image)
-# plt.show()
+print "Ref Area: ", refArea
+print "Obj Area: ", objArea
