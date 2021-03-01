da9150352ad3ab4d0619edc5b805addc90ace84b
dmackey199 <36615673+dmackey199@users.noreply.github.com>
2021-03-01 09:54:14 -0500

    cropping img

--- pixel-count/hole.py
+++ pixel-count/hole.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("mouseimg.png")
+img = cv2.imread("mouseimg_cropped.png")
 # cv2.imshow("original", img)
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
