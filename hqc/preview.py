ced0315562bad8626b64a5f8762185187e2f43f6
aaliyahrichlen <aaliyahrichlen@ufl.edu>
2021-03-11 22:10:28 -0500

    updated resolution

--- hqc/preview.py
+++ hqc/preview.py
@@ -1,10 +1,24 @@
 import picamera
+import argparse
 from time import sleep
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--fr", type=int, required=True,
+    help="set camera framerate, up to 90 fps")
+ap.add_argument("-s", "--secs", type=int, required=True,
+    help="time to record in seconds")
+args = vars(ap.parse_args())
+
 camera = picamera.PiCamera()
+if args["fr"] <= 30:
+    camera.resolution = (1920,1080) #30fps
+elif args["fr"] <= 60:
+    camera.resolution = (1280,720) #60fps
+else:
+    camera.resolution = (640,480) #90fps
 
 camera.start_preview()
-sleep(30)
+sleep(args["secs"])
 camera.stop_preview()
 
 camera.close()
