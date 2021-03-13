4468500edc10ba1fba4e742f22ffb75fe272c844
dmackey199 <36615673+dmackey199@users.noreply.github.com>
2021-03-13 17:59:13 -0500

    test contrast

--- pixel-count/crop.py
+++ pixel-count/crop.py
@@ -7,30 +7,44 @@ greedyCrop = [(177, 60), (281, 147)]
 def nothing(x):
   pass
 
-roi = cv2.imread("img1.png")
-gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-clone = gray.copy()
-#roi = clone[staticCrop[0][1]:staticCrop[1][1], staticCrop[0][0]:staticCrop[1][0]]
-roi = clone[greedyCrop[0][1]:greedyCrop[1][1], greedyCrop[0][0]:greedyCrop[1][0]]
-output = roi.copy()
-cv2.imshow("ROI", roi)
-cv2.waitKey(0)
+image = cv2.imread("img1.png")
 
-circles = cv2.HoughCircles(roi, cv2.HOUGH_GRADIENT, 1, 10)
-if circles is not None:
-	# convert the (x, y) coordinates and radius of the circles to integers
-	circles = np.round(circles[0, :]).astype("int")
-	# loop over the (x, y) coordinates and radius of the circles
-	for (x, y, r) in circles:
-		# draw the circle in the output image, then draw a rectangle
-		# corresponding to the center of the circle
-		cv2.circle(output, (x, y), r, (0, 255, 0), 4)
-		cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
-	# show the output image
-	cv2.imshow("output", np.hstack([roi, output]))
-	cv2.waitKey(0)
-else:
-  print("No Circles found")
+new_image = np.zeros(image.shape, image.dtype)
+alpha = 1.0;
+beta = 50;
+new_image = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+
+cv2.imshow('Original Image', image)
+cv2.imshow('New Image', new_image)
+# Wait until user press some key
+cv2.waitKey()
+
+# roi = cv2.imread("img1.png")
+# gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+# clone = gray.copy()
+# #roi = clone[staticCrop[0][1]:staticCrop[1][1], staticCrop[0][0]:staticCrop[1][0]]
+# roi = clone[greedyCrop[0][1]:greedyCrop[1][1], greedyCrop[0][0]:greedyCrop[1][0]]
+
+# cv2.imshow("ROI", roi)
+# cv2.waitKey(0)
+
+#HoughCircles
+# output = roi.copy()
+# circles = cv2.HoughCircles(roi, cv2.HOUGH_GRADIENT, 1, 10)
+# if circles is not None:
+# 	# convert the (x, y) coordinates and radius of the circles to integers
+# 	circles = np.round(circles[0, :]).astype("int")
+# 	# loop over the (x, y) coordinates and radius of the circles
+# 	for (x, y, r) in circles:
+# 		# draw the circle in the output image, then draw a rectangle
+# 		# corresponding to the center of the circle
+# 		cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+# 		cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+# 	# show the output image
+# 	cv2.imshow("output", np.hstack([roi, output]))
+# 	cv2.waitKey(0)
+# else:
+#   print("No Circles found")
 
 #thresholding
 # cv2.imshow("ROI", roi)
