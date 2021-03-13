dbf532ff272887a38c9e92105d6519eb0ca66681
dmackey199 <36615673+dmackey199@users.noreply.github.com>
2021-03-13 16:40:08 -0500

    convert to b&w

--- pixel-count/crop.py
+++ pixel-count/crop.py
@@ -8,9 +8,26 @@ def nothing(x):
   pass
 
 roi = cv2.imread("img1.png",0)
-clone = roi.copy()
+gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+clone = gray.copy()
 #roi = clone[staticCrop[0][1]:staticCrop[1][1], staticCrop[0][0]:staticCrop[1][0]]
 roi = clone[greedyCrop[0][1]:greedyCrop[1][1], greedyCrop[0][0]:greedyCrop[1][0]]
+circles = cv2.HoughCircles(roi, cv2.HOUGH_GRADIENT, 1.2, 100)
+
+# if circles is not None:
+# 	# convert the (x, y) coordinates and radius of the circles to integers
+# 	circles = np.round(circles[0, :]).astype("int")
+# 	# loop over the (x, y) coordinates and radius of the circles
+# 	for (x, y, r) in circles:
+# 		# draw the circle in the output image, then draw a rectangle
+# 		# corresponding to the center of the circle
+# 		cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+# 		cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+# 	# show the output image
+# 	cv2.imshow("output", np.hstack([image, output]))
+# 	cv2.waitKey(0)
+
+#thresholding
 cv2.imshow("ROI", roi)
 cv2.namedWindow('image')
 cv2.createTrackbar('min','image',0,255,nothing)
