f1ef524d219d04d28ef6af688852b8a8510f2568
dmackey199 <36615673+dmackey199@users.noreply.github.com>
2021-03-13 03:32:04 -0500

    crop

--- pixel-count/crop.py
+++ pixel-count/crop.py
@@ -4,6 +4,9 @@ import cv2
 staticCrop = [(151, 62), (581, 173)] 
 greedyCrop = [(177, 60), (281, 147)]
 
+def nothing(x):
+  pass
+
 roi = cv2.imread("img1.png",0)
 clone = roi.copy()
 roi = clone[staticCrop[0][1]:staticCrop[1][1], staticCrop[0][0]:staticCrop[1][0]]
