7d074001877dd919f9111ed4b41706dae283f456
dmackey199 <36615673+dmackey199@users.noreply.github.com>
2021-03-14 00:57:37 -0500

    .

--- pixel-count/crop.py
+++ pixel-count/crop.py
@@ -51,7 +51,7 @@ gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
 clone = gray.copy()
 #roi = clone[staticCrop[0][1]:staticCrop[1][1], staticCrop[0][0]:staticCrop[1][0]]
 roi = clone[greedyCrop[0][1]:greedyCrop[1][1], greedyCrop[0][0]:greedyCrop[1][0]]
-alpha = 1.3;
+alpha = 1.5;
 beta = 20;
 output = cv2.convertScaleAbs(roi, alpha=alpha, beta=beta)
 
