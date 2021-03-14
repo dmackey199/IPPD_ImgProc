314bf2872d14a21552103620d92e751693632e36
dmackey199 <36615673+dmackey199@users.noreply.github.com>
2021-03-14 01:05:35 -0500

    .

--- pixel-count/crop.py
+++ pixel-count/crop.py
@@ -52,7 +52,7 @@ clone = gray.copy()
 #roi = clone[staticCrop[0][1]:staticCrop[1][1], staticCrop[0][0]:staticCrop[1][0]]
 roi = clone[greedyCrop[0][1]:greedyCrop[1][1], greedyCrop[0][0]:greedyCrop[1][0]]
 alpha = 1.5;
-beta = 20;
+beta = 10;
 output = cv2.convertScaleAbs(roi, alpha=alpha, beta=beta)
 
 
