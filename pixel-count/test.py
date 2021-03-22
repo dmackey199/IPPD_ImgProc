74fe6bbffea85846b10e9d8bd3431254ca18e6c4
dmackey199 <36615673+dmackey199@users.noreply.github.com>
2021-03-21 23:41:39 -0400

    .

--- pixel-count/test.py
+++ pixel-count/test.py
@@ -82,7 +82,7 @@ contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPR
 chosen_contours = []
 for i in range(len(contours)):
     area = cv2.contourArea(contours[i])
-    if(area > 100 and area < 400):
+    if(area > 100):
         chosen_contours.append(contours[i])
         x1,y1,w1,h1 = cv2.boundingRect(contours[i])
         rect = cv2.rectangle(img, (x1, y1), (x1 + w1, y1 + h1), (36,255,12), 1)
