ac712724f41a1127db57273a89f8d679748e0cb3
dmackey199 <36615673+dmackey199@users.noreply.github.com>
2021-03-22 01:26:00 -0400

    .

--- pixel-count/test.py
+++ pixel-count/test.py
@@ -124,6 +124,9 @@ x,y,w,h = cv2.boundingRect(sorted_contours[0])
 rect = cv2.rectangle(roi, (x, y), (x + w, y + h), (36,255,12), 1)
 cv2.putText(rect, "EYE", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (36,255,12), 1)
 
+rect = cv2.rectangle(roi, refPt[0], (x -50, y + h), (36,255,12), 1)
+cv2.putText(rect, "ROI", (x, y+h+20), cv2.FONT_HERSHEY_SIMPLEX, 1, (36,255,12), 1)
+
 image = cv2.drawContours(roi, sorted_contours, -1, (0, 127, 0), 2)
 # show the image with the drawn contours
 while(1):
