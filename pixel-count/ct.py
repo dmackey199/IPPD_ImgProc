108ea8e50bf6c58e53815d5260dfc799e688d923
dmackey199 <36615673+dmackey199@users.noreply.github.com>
2021-03-30 20:04:08 -0400

    .

--- pixel-count/ct.py
+++ pixel-count/ct.py
@@ -17,16 +17,20 @@ hsvImg[...,2] = hsvImg[...,2]*0.7
 # img = cv2.imread("img4.png")
 # hsvImg = cv2.fastNlMeansDenoisingColored(img,None,10,10,7,21)
 
-cv2.namedWindow('refHole')
-cv2.createTrackbar('min','refHole',0,255,nothing)
-cv2.createTrackbar('max','refHole',0,255,nothing)
+# cv2.namedWindow('refHole')
+# cv2.createTrackbar('min','refHole',0,255,nothing)
+# cv2.createTrackbar('max','refHole',0,255,nothing)
 cv2.namedWindow('RefThreshold')
 while(1):
-    a = cv2.getTrackbarPos('min','refHole')
-    b = cv2.getTrackbarPos('max','refHole')
-    ret,thresh=cv2.threshold(hsvImg,a,b,cv2.THRESH_BINARY_INV)
-    cv2.imshow("RefThreshold",thresh)
-    # cv2.imshow("RefThreshold",hsvImg)
+    # a = cv2.getTrackbarPos('min','refHole')
+    # b = cv2.getTrackbarPos('max','refHole')
+    # ret,thresh=cv2.threshold(hsvImg,a,b,cv2.THRESH_BINARY_INV)
+    # cv2.imshow("RefThreshold",thresh)
+    # k = cv2.waitKey(10) & 0xFF
+    # if k == ord("c"):
+    #     break
+
+    cv2.imshow("RefThreshold",hsvImg)
     k = cv2.waitKey(10) & 0xFF
     if k == ord("c"):
       break
