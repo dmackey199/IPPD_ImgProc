0c449c6e15136da504d68ff71c768cf6983eb6ae
dmackey199 <36615673+dmackey199@users.noreply.github.com>
2021-03-30 21:34:10 -0400

    .

--- pixel-count/ct.py
+++ pixel-count/ct.py
@@ -2,6 +2,13 @@
 #https://answers.opencv.org/question/193276/how-to-change-brightness-of-an-image-increase-or-decrease/
 #https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_colorspaces/py_colorspaces.html
 
+# hMin = 69
+# hMax = 179
+# sMin = 40
+# sMax = 186
+# vMin = 52
+# vMax = 158
+
 import cv2
 import numpy as np
 # import matplotlib.pyplot as plt
@@ -21,19 +28,30 @@ hsvImg = cv2.fastNlMeansDenoisingColored(hsvImg,None,3,15,7,21)
 # cv2.createTrackbar('min','refHole',0,255,nothing)
 # cv2.createTrackbar('max','refHole',0,255,nothing)
 
-cv2.namedWindow('refHole')
-cv2.createTrackbar('hMin','refHole',0,179,nothing)
-cv2.createTrackbar('hMax','refHole',0,179,nothing)
-cv2.createTrackbar('sMin','refHole',0,255,nothing)
-cv2.createTrackbar('sMax','refHole',0,255,nothing)
-cv2.createTrackbar('vMin','refHole',0,255,nothing)
-cv2.createTrackbar('vMax','refHole',0,255,nothing)
-cv2.namedWindow('hsvImg')
-cv2.namedWindow('mask')
+# cv2.namedWindow('refHole')
+# cv2.createTrackbar('hMin','refHole',0,179,nothing)
+# cv2.createTrackbar('hMax','refHole',0,179,nothing)
+# cv2.createTrackbar('sMin','refHole',0,255,nothing)
+# cv2.createTrackbar('sMax','refHole',0,255,nothing)
+# cv2.createTrackbar('vMin','refHole',0,255,nothing)
+# cv2.createTrackbar('vMax','refHole',0,255,nothing)
+# cv2.namedWindow('hsvImg')
+# cv2.namedWindow('mask')
 
 # cv2.namedWindow('refHole')
 # cv2.createTrackbar('h','refHole',0,20,nothing)
 # cv2.createTrackbar('hColor','refHole',0,20,nothing)
+
+hMin = 69
+hMax = 179
+sMin = 40
+sMax = 186
+vMin = 52
+vMax = 158
+lowerBound = np.array([hMin,sMin,vMin])
+upperBound = np.array([hMax,sMax,vMax])
+mask = cv2.inRange(hsvImg, lowerBound, upperBound)
+invert = cv2.bitwise_not(mask)
 
 
 cv2.namedWindow('RefThreshold')
@@ -46,22 +64,22 @@ while(1):
     # if k == ord("c"):
     #     break
 
-    hMin = cv2.getTrackbarPos('hMin','refHole')
-    hMax = cv2.getTrackbarPos('hMax','refHole')
-    sMin = cv2.getTrackbarPos('sMin','refHole')
-    sMax = cv2.getTrackbarPos('sMax','refHole')
-    vMin = cv2.getTrackbarPos('vMin','refHole')
-    vMax = cv2.getTrackbarPos('vMax','refHole')
-    lowerBound = np.array([hMin,sMin,vMin])
-    upperBound = np.array([hMax,sMax,vMax])
-    mask = cv2.inRange(hsvImg, lowerBound, upperBound)
-    res = cv2.bitwise_and(img,img, mask=mask)
-    cv2.imshow("RefThreshold",res)
-    cv2.imshow("hsvImg",hsvImg)
-    cv2.imshow("mask",mask)
-    k = cv2.waitKey(10) & 0xFF
-    if k == ord("c"):
-      break
+    # hMin = cv2.getTrackbarPos('hMin','refHole')
+    # hMax = cv2.getTrackbarPos('hMax','refHole')
+    # sMin = cv2.getTrackbarPos('sMin','refHole')
+    # sMax = cv2.getTrackbarPos('sMax','refHole')
+    # vMin = cv2.getTrackbarPos('vMin','refHole')
+    # vMax = cv2.getTrackbarPos('vMax','refHole')
+    # lowerBound = np.array([hMin,sMin,vMin])
+    # upperBound = np.array([hMax,sMax,vMax])
+    # mask = cv2.inRange(hsvImg, lowerBound, upperBound)
+    # res = cv2.bitwise_and(img,img, mask=mask)
+    # cv2.imshow("RefThreshold",res)
+    # cv2.imshow("hsvImg",hsvImg)
+    # cv2.imshow("mask",mask)
+    # k = cv2.waitKey(10) & 0xFF
+    # if k == ord("c"):
+    #   break
 
     # h = cv2.getTrackbarPos('h','refHole')
     # hColor = cv2.getTrackbarPos('hColor','refHole')
@@ -71,10 +89,10 @@ while(1):
     # if k == ord("c"):
     #     break
 
-    # cv2.imshow("RefThreshold",hsvImg)
-    # k = cv2.waitKey(10) & 0xFF
-    # if k == ord("c"):
-    #   break
+    cv2.imshow("RefThreshold",invert)
+    k = cv2.waitKey(10) & 0xFF
+    if k == ord("c"):
+      break
 
 
 
